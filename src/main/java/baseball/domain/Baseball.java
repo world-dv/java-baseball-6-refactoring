fc493fe7afe1251148ff904e824095cc799bc475
world-dv <goodforx0147@naver.com>
2024-10-09 18:34:10 +0900

    feat: create and check number of the input

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,5 +1,28 @@
 package baseball.domain;
 
+import baseball.exception.NumberException;
+
 public class Baseball {
-    private String number;
+
+    private final String number;
+
+    public Baseball(String number) {
+        if (!checkNumberLength(number) || !checkIsNumber(number)) {
+            throw new IllegalArgumentException(NumberException.INPUT_EXCEPTION.getMessage());
+        }
+        this.number = number;
+    }
+
+    public boolean checkNumberLength(String number) {
+        return number.length() == 3;
+    } //입력이 1에서 9가 아닐 경우, 입력이 3개가 아닌 경우
+
+    public boolean checkIsNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    } //입력이 수가 아닐 경우
 }
