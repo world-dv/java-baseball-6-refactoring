5607770b33eae70dee8ecfd61aab1c908ab71d91
world-dv <goodforx0147@naver.com>
2024-10-09 17:19:20 +0900

    feat: add static to use view

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String inputUser(String message) {
+    public static String inputUser(String message) {
         System.out.println(message);
         return Console.readLine();
     } //INPUT, RESTART
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,15 +2,15 @@ package baseball.view;
 
 public class OutputView {
 
-    public void printMessage(String message) {
+    public static void printMessage(String message) {
         System.out.println(message);
     } //print START, NOTHING, SUCCESS, END
 
-    public void printResult(String s, String message) {
+    public static void printResult(String s, String message) {
         System.out.println(s + message);
     } //STRIKE, BALL
 
-    public void printBallAndStrike(String b, String s) {
+    public static void printBallAndStrike(String b, String s) {
         System.out.println(b + Message.BALL + " " + s + Message.STRIKE);
     }
 }
