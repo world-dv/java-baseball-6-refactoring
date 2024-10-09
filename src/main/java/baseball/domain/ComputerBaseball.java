72d1a56e3c6366536e967773adb323509cf62853
world-dv <goodforx0147@naver.com>
2024-10-09 19:14:08 +0900

    feat: create random number of computer

---
+++ src/main/java/baseball/domain/ComputerBaseball.java
@@ -0,0 +1,31 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ComputerBaseball {
+
+    private final String computerNumber;
+
+    public ComputerBaseball() {
+        this.computerNumber = createComputerNumber();
+    }
+
+    public String createComputerNumber() {
+        Set<Integer> computer = new HashSet<>();
+        while (computer.size() < 3) {
+            computer.add(createRandomNumber());
+        }
+        return changeListToString(computer);
+    }
+
+    public Integer createRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+    public String changeListToString(Set<Integer> list) {
+        return list.toString();
+    }
+}
