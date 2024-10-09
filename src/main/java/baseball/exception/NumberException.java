740ea094aa5f185b39862afcb7c1b5fb4eb77bf3
world-dv <goodforx0147@naver.com>
2024-10-09 21:35:30 +0900

    feat: add end number exception message

--- src/main/java/baseball/exception/NumberException.java
+++ src/main/java/baseball/exception/NumberException.java
@@ -2,7 +2,8 @@ package baseball.exception;
 
 public enum NumberException {
 
-    INPUT_EXCEPTION("1에서 9사이 중복되지 않는 정수만 입력해주세요.");
+    INPUT_EXCEPTION("1에서 9사이 중복되지 않는 정수만 입력해주세요."),
+    END_INPUT_EXCEPTION("1 또는 2를 입력해주세요.");
 
     private final String message;
 
