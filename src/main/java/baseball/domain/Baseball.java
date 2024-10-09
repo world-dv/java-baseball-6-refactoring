9b59a73f5d846d1c93a61998f5a0f4b54007750e
world-dv <goodforx0147@naver.com>
2024-10-09 18:48:19 +0900

    feat: check duplication of number

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -2,12 +2,15 @@ package baseball.domain;
 
 import baseball.exception.NumberException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Baseball {
 
     private final String number;
 
     public Baseball(String number) {
-        if (!checkNumberLength(number) || !checkIsNumber(number)) {
+        if (!checkNumberLength(number) || !checkIsNumber(number) || !checkDuplicationNumber(number)) {
             throw new IllegalArgumentException(NumberException.INPUT_EXCEPTION.getMessage());
         }
         this.number = number;
@@ -25,4 +28,12 @@ public class Baseball {
             return false;
         }
     } //입력이 수가 아닐 경우
+
+    public boolean checkDuplicationNumber(String number) {
+        Set<Character> numberSet = new HashSet<>();
+        for (int i = 0; i < number.length(); i++) {
+            numberSet.add(number.charAt(i));
+        }
+        return numberSet.size() == 3;
+    }
 }
