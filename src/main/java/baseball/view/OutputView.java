8919747aa9eb9ea49936c66518247011959d9645
world-dv <goodforx0147@naver.com>
2024-10-09 21:09:11 +0900

    feat: print messages

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
         System.out.println(message.getMessage());
     } //print START, NOTHING, SUCCESS, END
 
+    public static void printMessages(Message message1, Message message2) {
+        System.out.println(message1.getMessage() + " " + message2.getMessage());
+    }
+
     public static void printResult(String s, Message message) {
         System.out.println(s + message.getMessage());
     } //STRIKE, BALL
